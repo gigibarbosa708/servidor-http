bbe7d2f503c0d843bd90351dd69ec066ab01bd4b
gigibarbosa708 <giovana.barbosa@estudante.ifms.edu.br>
2026-08-14 21:27:19 +0000

    feat: add three new routes

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,19 +1,37 @@
 import http from 'node:http'
+import { URL } from 'node:url'
 
-const PORT = 3000
+const porta = 3000
 
-const server = http.createServer();
+const server = http.createServer()
 
-server.on('request', (req, res) => {
-const date = new Date().toISOString()
+const requisicao = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
 
-console.log(`Requesição recebida! ${req.method} ${req.url} ${date}`);
+    if (req.method === 'GET' && urlObj.pathname === '/saudacao') {
+        const nome = urlObj.searchParams.get('nome');
+        return res.end(JSON.stringify({"nome": nome}));
+    } else if (req.method === 'GET' && urlObj.pathname === '/') {
+        return res.end(JSON.stringify({"data": "Você está na página inicial!"}));
+    } else if (req.method === 'GET' && urlObj.pathname === '/contato') {
+        return res.end(JSON.stringify({"data": "Entre em contato com nosso suporte através do número 0800 984 652"}));
+    } else if (req.method === 'GET' && urlObj.pathname === '/produtos') {
+        return res.end(JSON.stringify(
+            {"data":
+              [{"título": "Bare Vanilla", "preço": 160.99 },{"título": "Velvet Petals", "preço": 350.00 }, {"título": "Love Spell", "preço": 199.19 } ]}));
+    }
 
-res.statusCode = 201
-res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-res.end('Recurso criado');
+    return res.end(JSON.stringify({ "chave": "valor" }));
+
+
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    res.end();
+}
+
+server.on('request', requisicao);
+
+server.listen(porta, () => {
+    console.log(`Servidor ouvindo na porta ${porta}`)
 });
-
-server.listen(PORT, ()=> {
-console.log(`Servirdor rodando na porta${PORT}`)
-})
