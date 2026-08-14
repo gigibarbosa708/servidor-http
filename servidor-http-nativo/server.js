b1cebc5500abfc724793666cbf9a68fe14dfddce
gigibarbosa708 <giovana.barbosa@estudante.ifms.edu.br>
2026-08-14 20:49:07 +0000

    feat: new feature add

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -5,13 +5,15 @@ const PORT = 3000
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    console.log('Requesição recebida! ${req.method} ${req.url}');
+const date = new Date().toISOString()
 
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Recurso criado');
+console.log(`Requesição recebida! ${req.method} ${req.url} ${date}`);
+
+res.statusCode = 201
+res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+res.end('Recurso criado');
 });
 
 server.listen(PORT, ()=> {
-    console.log(`Servirdor rodando na porta${PORT}`)
+console.log(`Servirdor rodando na porta${PORT}`)
 })
